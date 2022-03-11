32498a07a8988bffae445d8748f0acf6d8b43d4d
Wojciech <wojciech.mista@hotmail.com>
2022-03-11 12:54:12 +0100

    Remove explicit ref type

--- src/components/Form/ExitFormDialogProvider.tsx
+++ src/components/Form/ExitFormDialogProvider.tsx
@@ -58,7 +58,7 @@ const ExitFormDialogProvider = ({ children }) => {
   const { history: routerHistory } = useRouter();
 
   const [showDialog, setShowDialog] = useState(defaultValues.showDialog);
-  const isSubmitDisabled = useRef<boolean>(false);
+  const isSubmitDisabled = useRef(false);
 
   const setIsSubmitDisabled = (status: boolean) => {
     isSubmitDisabled.current = status;
